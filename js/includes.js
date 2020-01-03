f806fec20abc2e2435bcf8214eff0b172c4d2db8
Khinshan Khan <khanshan501@gmail.com>
2020-01-03 08:24:25 -0500

    generalized some functions and added comments to includes.js

--- js/includes.js
+++ js/includes.js
@@ -1,4 +1,7 @@
+// holds mappings of "fn_name" <-> fn
 var context = {};
+
+// injection for navbar
 const navbar = () => {
   return `
     <nav class="navbar">
@@ -13,28 +16,23 @@ const navbar = () => {
     </nav>
 `;
 };
+
 context["navbar"] = navbar;
 
+// injection for footer
 const footer = () => {
-  return `
-    <nav class="navbar">
-      <a href="https://mtlynch3.github.io/savethewhales" class="logo">
-        <img src="http://www.pngmart.com/files/7/Killer-Whale-PNG-File.png" alt="savethewhales">
-      </a>
-      <ul class="nav-links">
-        <li class="nav-item"><a href="#">Home</a></li>
-        <li class="nav-item"><a href="#">About us</a></li>
-        <li class="nav-item"><a href="#">Donate</a></li>
-      </ul>
-    </footer>
-`;
+  return `footer`;
 };
 
+context["footer"] = footer;
+
+// sets html with the proper injection
 const set_includes = elem => {
   const e = document.getElementById(elem);
-  e.innerHTML = context["navbar"]();
+  e.innerHTML = context[elem]();
 };
 
+// map set_includes onto the page's chosen elements
 const includes = elems => {
   elems.forEach(elem => set_includes(elem));
 };
